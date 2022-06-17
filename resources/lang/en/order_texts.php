78196c398ef3f70f75257cdd1e24f12aa3c7de73
opatrickcarvalho <suporte.patrickcarvalho@gmail.com>
2022-06-16 22:11:48 -0300

    initial commit

---
+++ resources/lang/en/order_texts.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'searching_for_delivery_man'=>'Procurando por entregador',
+    'accepted_by_delivery_man'=>'Aceito pelo entregador',
+    'preparing_in_restaurants'=>'Preparação em restaurantes',
+    'picked_up'=>'Retirada',
+    'dashboard_order_statistics'=>'Estatísticas de pedidos do painel',
+];
